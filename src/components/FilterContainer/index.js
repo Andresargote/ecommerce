5c8de3b4a992a7259af27ad00de09493bb953d2d
Andresargote <andresargote2019@gmail.com>
2022-06-05 16:53:21 +0200

    Added fakeProducts mock

--- src/components/FilterContainer/index.js
+++ src/components/FilterContainer/index.js
@@ -3,11 +3,11 @@ import styles from './FilterContainer.module.css';
 
 const FilterContainer = ({ categories }) => {
   return (
-    <div className={styles.filterContainer}>
+    <section className={styles.filterContainer}>
       {categories.map((category) => (
         <Filter key={category.id} category={category.name} />
       ))}
-    </div>
+    </section>
   );
 };
 
